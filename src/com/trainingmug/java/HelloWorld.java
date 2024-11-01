0fc212351908c3b0ed65bdff1577411d53017753
rajagopalreddy-tm <rajagopal.reddy@trainingmug.com>
2024-11-01 13:20:52 +0530

    helloworld-program

---
+++ src/com/trainingmug/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.trainingmug.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to Java Programing");
+        System.out.println("Java is Awesome !!");
+       
+    }
+}
